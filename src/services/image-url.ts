c6df9b8ad7a64120c30b7c13ce9facc6b18eaa14
bardouhd <daniel.bardouh@mirvac.com>
2023-07-17 22:36:41 +1000

    handle games without images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
   if (url) return url.replace("/media/", "/media/crop/600/400/");
-  else return url;
+  else return noImage;
 };
 
 export default getCroppedImageUrl;
